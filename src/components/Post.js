a27db930c07de7bbd1a93f02f3f0f5456389e56e
Joshua Knudsen <knudsen.joshua772@gmail.com>
2023-01-26 13:08:20 -0800

    Styling home feed

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,5 @@
 import { ChatBubbleOutline, FavoriteBorder, IosShare, MoreHoriz, Repeat } from '@mui/icons-material'
-import { Avatar, Menu, Tooltip, Typography } from '@mui/material'
+import { Avatar, Tooltip, Typography } from '@mui/material'
 import React from 'react'
 
 const Post = ({postDetails}) => {
@@ -7,18 +7,18 @@ const Post = ({postDetails}) => {
     <div className='post-div'>
         {/* Will be a conditional statement determining if the post was reshared (repollenated) by a user they follow. If so then will have a little script at the top containing who did */}
         <Avatar />
+        <div className='post-content-wrapper-div'>
         <div className='post-header-div'>
-            <Typography variant='h5'>Display Name</Typography>
+            <h5 className='post-display-name'>Display Name</h5>
             {/* Verified Badge? */}
-            <Typography variant='h6'>UserName</Typography>
-            <Typography variant='body1'>TimeStamp</Typography>
+            <h7 className='post-user-name'>UserName</h7>
+            <h7 className='post-time-stamp-separator'> - </h7>
+            <h7 className='post-time-stamp'>TimeStamp</h7>
         </div>
-
-        <Tooltip><MoreHoriz /></Tooltip>
 
         {/* If it is a reply then it will say it is replying to a user */}
     
-        <Typography variant='subtitle1'>Content</Typography>
+        <Typography variant='subtitle1' className='post-content'>Content</Typography>
 
         <div className='post-tools-div'>
             <Tooltip title='Reply'><ChatBubbleOutline /></Tooltip>
@@ -26,6 +26,10 @@ const Post = ({postDetails}) => {
             <Tooltip title='Like'><FavoriteBorder /></Tooltip>
             <Tooltip title='Share'><IosShare /></Tooltip>
         </div>
+
+        </div>
+
+        <Tooltip title='More'><MoreHoriz /></Tooltip>
     
     </div>
   )
