0951ebffc962b9e790351787505ad34a861c7c0b
Joshua Knudsen <knudsen.joshua772@gmail.com>
2023-01-26 16:03:17 -0800

    More styling for the home feed

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,5 @@
 import { ChatBubbleOutline, FavoriteBorder, IosShare, MoreHoriz, Repeat } from '@mui/icons-material'
-import { Avatar, Tooltip, Typography } from '@mui/material'
+import { Avatar, Tooltip } from '@mui/material'
 import React from 'react'
 
 const Post = ({postDetails}) => {
@@ -9,27 +9,34 @@ const Post = ({postDetails}) => {
         <Avatar />
         <div className='post-content-wrapper-div'>
         <div className='post-header-div'>
+          <div className='post-header-details-div'>
             <h5 className='post-display-name'>Display Name</h5>
             {/* Verified Badge? */}
             <h7 className='post-user-name'>UserName</h7>
             <h7 className='post-time-stamp-separator'> - </h7>
             <h7 className='post-time-stamp'>TimeStamp</h7>
+          
+          </div>
+            
+            
+            <Tooltip title='More' className='post-settings-button'><MoreHoriz /></Tooltip>
+        
         </div>
 
         {/* If it is a reply then it will say it is replying to a user */}
     
-        <Typography variant='subtitle1' className='post-content'>Content</Typography>
+        <p className='post-content'>Content</p>
 
         <div className='post-tools-div'>
-            <Tooltip title='Reply'><ChatBubbleOutline /></Tooltip>
-            <Tooltip title='Repollinate'><Repeat /></Tooltip>
-            <Tooltip title='Like'><FavoriteBorder /></Tooltip>
-            <Tooltip title='Share'><IosShare /></Tooltip>
+            <Tooltip title='Reply' className='post-reply-tooltip'><ChatBubbleOutline /></Tooltip>
+            <Tooltip title='Repollinate' className='post-repollinate-tooltip'><Repeat /></Tooltip>
+            <Tooltip title='Like' className='post-like-tooltip'><FavoriteBorder /></Tooltip>
+            <Tooltip title='Share' className='post-reply-tooltip'><IosShare /></Tooltip>
         </div>
 
         </div>
 
-        <Tooltip title='More'><MoreHoriz /></Tooltip>
+        
     
     </div>
   )
