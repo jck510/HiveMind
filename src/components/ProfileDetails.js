8e0ecae2dff06090fc8e552c247111334d854e02
Joshua Knudsen <knudsen.joshua772@gmail.com>
2023-02-03 13:32:53 -0800

    Began styling the profile page section and added hover styling for posts

--- src/components/ProfileDetails.js
+++ src/components/ProfileDetails.js
@@ -6,17 +6,22 @@ import React from 'react'
 const ProfileDetails = () => {
   return (
     <div className='profile-details-div'>
-        <div className='profile-details-header-div'>
-          <KeyboardBackspace />
-          <Typography variant='h5'>Display Name</Typography>
+        
+        <img alt='' src='../assets/images/samplebanner.jpg' className='profile-details-banner-picture' height='200px' width='595px' />
+        
+        <div className='profile-details-profile-picture-and-button-div'>
+        <Avatar src='' className='profile-details-profile-picture' sx={{height: '140px', width: '140px' , outline: '5px solid white', marginLeft: '20px', marginTop: '-75px'}} ></Avatar>
+        
+        <div className='profile-details-button-div'>
+
+          {/* Will show edit profile button if the user logged in is the same as the profile that is being viewed */}
+          <button>Edit profile</button>
+          {/* Otherwise there will be a follow button */}
+          <Button variant='contained'>Follow</Button>
 
         </div>
-        <Avatar src='' className='profile-details-profile-picture'></Avatar>
-        <image src='' className='profile-details-banner-picture' />
-        {/* Will show edit profile button if the user logged in is the same as the profile that is being viewed */}
-        <button>Edit profile</button>
-        {/* Otherwise there will be a follow button */}
-        <Button variant='contained'>Follow</Button>
+
+        </div>
         <h3>Display Name</h3>
         <Verified />
         <h5>Username</h5>
--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,3 +1,4 @@
+import { KeyboardBackspace } from '@mui/icons-material'
 import React from 'react'
 import ProfileDetails from './ProfileDetails'
 import ProfileDetailsFeed from './ProfileDetailsFeed'
@@ -5,6 +6,11 @@ import ProfileDetailsFeed from './ProfileDetailsFeed'
 const ProfilePage = () => {
   return (
     <div className='profile-page-div'>
+      <div className='profile-page-header-div home-page-header-div'>
+          <KeyboardBackspace />
+          <h2>Display Name</h2>
+
+      </div>
         <ProfileDetails />
         <ProfileDetailsFeed />
     </div>
